b54e3e119a4845c2c1b39aeb2befdbf678566b2f
Pablo-Carreras <pablo.carreras-canton@mths.ca>
2024-11-07 10:34:39 -0500

    made coee

--- main.ts
+++ main.ts
@@ -1,8 +1,40 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
+ * Created by: Pablo Carreras
+ * Created on: Nov 2024
  * This program ...
 */
 
-basic.showString('Hello, World!')
+// variables
+let sprite: game.LedSprite = null
+let loopCounter = 0
+let moveCounter = 0
+
+// setup
+basic.showIcon(IconNames.Happy)
+basic.pause(500)
+basic.clearScreen()
+
+// When "A" is pressed
+input.onButtonPressed(Button.A, function () {
+    // setup
+    basic.clearScreen()
+    loopCounter = 0
+    sprite = game.createSprite(0, 0)
+    moveCounter = 0
+
+    // Move the sprite around the screen
+    while (loopCounter < 4) {
+        moveCounter = 0
+        while (moveCounter < 5) {
+            moveCounter = moveCounter + 1
+            pause(500)
+            sprite.move(1)
+           
+        }
+
+        // turn 90 degrees and add 1 to the loopCounter
+        sprite.turn(Direction.Right, 90)
+        loopCounter = loopCounter + 1
+    }
+})
